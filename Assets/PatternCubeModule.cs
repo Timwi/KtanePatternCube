5d6fa1b432774cb02472b626cc1f7e615ab36df6
Timwi <github@timwi.de>
2018-11-09 23:14:07 +0100

    Rule-seed support.

--- Assets/PatternCubeModule.cs
+++ Assets/PatternCubeModule.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using PatternCube;
 using UnityEngine;
@@ -19,6 +17,7 @@ public class PatternCubeModule : MonoBehaviour
     public KMBombModule Module;
     public KMAudio Audio;
     public KMSelectable MainSelectable;
+    public KMRuleSeedable RuleSeedable;
 
     public Mesh[] NetMeshes;
     public KMSelectable[] SelectableBoxes;
@@ -43,9 +42,9 @@ public class PatternCubeModule : MonoBehaviour
     private int _faceGivenByHighlight;
     private int _selected = 2;
     private int _highlightedPosition;   // for Souvenir
+
     private readonly List<int> _facesRevealed = new List<int>();
-    private Dictionary<char, Texture> _symbolTextures;
-    private char[] _tpLetters = new char[6];
+    private readonly char[] _tpLetters = new char[6];
 
     private sealed class RotateTask { public int From; public int To; }
     private readonly Queue<RotateTask>[] _queues = new Queue<RotateTask>[5];
@@ -78,26 +77,71 @@ public class PatternCubeModule : MonoBehaviour
             _placeableScreenObjs[i] = PlaceableBoxes[i].transform.Find("Screen").GetComponent<MeshRenderer>();
         }
 
-        _symbolTextures = new Dictionary<char, Texture>();
-        foreach (var tx in SymbolTextures)
-            _symbolTextures[tx.name[6]] = tx;
-
         for (int i = 0; i < 25; i++)
             if (i % 5 != 0)
                 MainSelectable.Children[i] = null;
+
+        // Generate the rules
+        List<int> symbolIxs;
+        var rnd = RuleSeedable.GetRNG();
+        string[] groupStrs = new string[2];
+        if (rnd.Seed == 1)
+        {
+            groupStrs[0] =
+                "X1,A3,B1;Y0,X1,A1;B0,D1,C2;Z1,A2,X0;X2,C2,B1;Y3,C1,B0;" +
+                "Y2,B0,A2;X1,Y2,B0;C2,A3,D1;Z1,X3,B1;D0,B0,X0;D1,B2,Y0;" +
+                "X2,C0,A1;A2,C3,Y2;X2,C3,Y0;D3,A0,B3;X0,Z2,C1;X1,C1,D0;" +
+                "X1,A2,D2;A0,Y3,D0;Y0,D0,X3;C3,B3,A1;Z1,D0,X0;C1,D0,Y2";
+            groupStrs[1] =
+                "X1,E0,F0;X0,E3,Y1;H1,G1,F1;Y3,Z3,E1;G1,X1,F0;G1,Z1,F1;" +
+                "Z3,F1,E2;X0,F0,Y2;G1,E2,H3;F1,Y2,Z0;X3,H1,F0;F1,Z1,H2;" +
+                "E0,X2,G1;G2,E2,Z1;Y1,X3,G3;H1,E3,F0;G0,Z2,Y0;G1,H3,X0;" +
+                "H3,X2,E2;Z0,H2,E3;X3,H3,Y0;E0,F0,G1;Z3,Y3,H1;Z2,H3,G2";
+            symbolIxs = new List<int> { 90, 91, 92, 93, 100, 101, 102, 103, 110, 111, 112 };
+        }
+        else
+        {
+            var skip = rnd.Next(0, 100);
+            for (var i = 0; i < skip; i++)
+                rnd.NextDouble();
+
+            symbolIxs = Enumerable.Range(0, 120).ToList();
+            rnd.ShuffleFisherYates(symbolIxs);
+            var arrs = new List<string>();
+            var groupsTemplates = new[] {
+                "ABX,AXY,BCD,AXZ,BCX,BCY,ABY,BXY,ACD,BXZ,BDX,BDY,ACX,ACY,CXY,ABD,CXZ,CDX,ADX,ADY,DXY,ABC,DXZ,CDY",
+                "EFX,EXY,FGH,EYZ,FGX,FGZ,EFZ,FXY,EGH,FYZ,FHX,FHZ,EGX,EGZ,GXY,EFH,GYZ,GHX,EHX,EHZ,HXY,EFG,HYZ,GHZ"
+            };
+            for (var i = 0; i < 2; i++)
+            {
+                var grs = groupsTemplates[i].Split(',');
+                var arr = new List<string>();
+                for (var j = 0; j < grs.Length; j++)
+                {
+                    var smbs = grs[j].Select(ch => ch.ToString()).ToList();
+                    rnd.ShuffleFisherYates(smbs);
+                    for (var k = 0; k < smbs.Count; k++)
+                        smbs[k] = smbs[k] + rnd.Next(0, 4);
+                    arr.Add(smbs.JoinString(","));
+                }
+                groupStrs[i] = arr.JoinString(";");
+            }
+        }
+
+        HalfCube[][] groups = groupStrs.Select(str => str.Split(';').Select(hci => hci.Split(',').Select(fc => new FaceSymbol(fc[0], fc[1] - '0')).ToArray()).Select(arr => new HalfCube(arr[0], arr[1], arr[2])).ToArray()).ToArray();
 
         // Generate a puzzle
         _puzzle = Data.Nets[Rnd.Range(0, Data.Nets.Length)];
         HalfCube[] frontHalfCubes, backHalfCubes;
         if (Rnd.Range(0, 2) == 0)
         {
-            frontHalfCubes = Data.Group1;
-            backHalfCubes = Data.Group2;
+            frontHalfCubes = groups[0];
+            backHalfCubes = groups[1];
         }
         else
         {
-            frontHalfCubes = Data.Group2;
-            backHalfCubes = Data.Group1;
+            frontHalfCubes = groups[1];
+            backHalfCubes = groups[0];
         }
         var frontHalfCube = frontHalfCubes[Rnd.Range(0, frontHalfCubes.Length)];
         var symbolsAlready = new[] { frontHalfCube.Top.Symbol, frontHalfCube.Left.Symbol, frontHalfCube.Front.Symbol };
@@ -137,7 +181,7 @@ public class PatternCubeModule : MonoBehaviour
         // Populate the selectable boxes
         for (int i = 0; i < 5; i++)
         {
-            _selectableSymbolObjs[i].material.mainTexture = _symbolTextures[_selectableSymbols[i].Symbol];
+            _selectableSymbolObjs[i].material.mainTexture = SymbolTextures[symbolIxs["ABCDEFGHXYZ".IndexOf(_selectableSymbols[i].Symbol)]];
             _selectableSymbolObjs[i].transform.localEulerAngles = new Vector3(90, _selectableSymbols[i].Orientation * 90, 0);
             _selectableFrameObjs[i].material = FrameRed;
             _selectableScreenObjs[i].material = _selectableSymbols[i].Symbol == _solution[_faceGivenByHighlight].Symbol ? ScreenLight : ScreenDark;
@@ -159,7 +203,7 @@ public class PatternCubeModule : MonoBehaviour
                     continue;
                 var fi = _puzzle.Faces[x, y];
                 _placeableSymbolObjs[fi.Face].transform.localEulerAngles = new Vector3(90, (fi.Orientation + _solution[fi.Face].Orientation) * 90, 0);
-                _placeableSymbolObjs[fi.Face].material.mainTexture = _symbolTextures[_solution[fi.Face].Symbol];
+                _placeableSymbolObjs[fi.Face].material.mainTexture = SymbolTextures[symbolIxs["ABCDEFGHXYZ".IndexOf(_solution[fi.Face].Symbol)]];
                 _placeableSymbolObjs[fi.Face].gameObject.SetActive(fi.Face == faceGivenFully);
                 _placeableFrameObjs[fi.Face].material = fi.Face == faceGivenFully ? FrameGreen : FrameYellow;
                 _placeableScreenObjs[fi.Face].material = fi.Face == _faceGivenByHighlight ? ScreenLight : ScreenDark;
@@ -178,7 +222,7 @@ public class PatternCubeModule : MonoBehaviour
             /* {0} */ 120 * _puzzle.Faces.GetLength(1) + 6,
             /* {1} */ Enumerable.Range(0, _puzzle.Faces.GetLength(1)).SelectMany(y => Enumerable.Range(0, _puzzle.Faces.GetLength(0)).Select(x =>
                 _puzzle.Faces[x, y] == null ? null :
-                _puzzle.Faces[x, y].Face == faceGivenFully ? svg(x, y, _solution[_puzzle.Faces[x, y].Face].Symbol, (_solution[_puzzle.Faces[x, y].Face].Orientation + _puzzle.Faces[x, y].Orientation) % 4) :
+                _puzzle.Faces[x, y].Face == faceGivenFully ? svg(x, y, symbolIxs["ABCDEFGHXYZ".IndexOf(_solution[_puzzle.Faces[x, y].Face].Symbol)], (_solution[_puzzle.Faces[x, y].Face].Orientation + _puzzle.Faces[x, y].Orientation) % 4) :
                 svg(x, y, highlighted: _puzzle.Faces[x, y].Face == _faceGivenByHighlight))).JoinString());
 
         Log(@"=svg[Symbols:]<svg xmlns='http://www.w3.org/2000/svg' viewBox='-3 -3 1206 126'>{0}</svg>",
@@ -189,7 +233,7 @@ public class PatternCubeModule : MonoBehaviour
             /* {0} */ 120 * _puzzle.Faces.GetLength(1) + 6,
             /* {1} */ Enumerable.Range(0, _puzzle.Faces.GetLength(1)).SelectMany(y => Enumerable.Range(0, _puzzle.Faces.GetLength(0)).Select(x =>
                 _puzzle.Faces[x, y] == null ? null :
-                svg(x, y, _solution[_puzzle.Faces[x, y].Face].Symbol, (_solution[_puzzle.Faces[x, y].Face].Orientation + _puzzle.Faces[x, y].Orientation) % 4))).JoinString());
+                svg(x, y, symbolIxs["ABCDEFGHXYZ".IndexOf(_solution[_puzzle.Faces[x, y].Face].Symbol)], (_solution[_puzzle.Faces[x, y].Face].Orientation + _puzzle.Faces[x, y].Orientation) % 4))).JoinString());
 
         // Because TP doesn’t set TwitchPlaysActive soon enough, we have to delay the TP letter initialization
         StartCoroutine(initialize());
@@ -202,45 +246,15 @@ public class PatternCubeModule : MonoBehaviour
         yield break;
     }
 
-    private static string svg(int x, int y, char symbol = '\0', int orientation = -1, bool highlighted = false)
+    private static string svg(int x, int y, int? symbol = null, int orientation = -1, bool highlighted = false)
     {
         var rect = string.Format("<rect x='{0}' y='{1}' width='120' height='120' fill='{2}' stroke='black' stroke-width='2'/>", x * 120, y * 120, highlighted ? "#f88" : "none");
-        if (symbol == '\0')
-            return rect;
-        return rect + svgPath(x, y, symbol, orientation);
+        return symbol == null ? rect : rect + svgPath(x, y, symbol.Value, orientation);
     }
-    private static string svgPath(int x, int y, char symbol, int orientation)
+    private static string svgPath(int x, int y, int symbol, int orientation)
     {
-        var sy = _svgSymbols[symbol];
-        return string.Format("<path d='{5}' transform='translate({3}, {4}) rotate({2}) translate({0}, {1})'/>", -sy.X - 60, -sy.Y - 60, 90 * orientation, 120 * x + 60, 120 * y + 60, sy.SvgPath);
+        return string.Format("<path d='{5}' transform='translate({3}, {4}) rotate({2}) translate({0}, {1})'/>", -120 * (symbol % 10) - 60, -120 * (symbol / 10) - 60, 90 * orientation, 120 * x + 60, 120 * y + 60, Data.Svgs[symbol]);
     }
-
-    sealed class SvgSymbol
-    {
-        public string SvgPath { get; private set; }
-        public int X { get; private set; }
-        public int Y { get; private set; }
-        public SvgSymbol(int x, int y, string svgPath)
-        {
-            SvgPath = svgPath;
-            X = x;
-            Y = y;
-        }
-    }
-
-    private static readonly Dictionary<char, SvgSymbol> _svgSymbols = new Dictionary<char, SvgSymbol> {
-        { 'A', new SvgSymbol(0, 0, "M60 10a50 50 0 1 0 0 100 50 50 0 0 0 0-100zm0 10a40 40 0 0 1 40 40H20a40 40 0 0 1 40-40z") },
-        { 'B', new SvgSymbol(120, 0, "M180 10l-50 100h100z") },
-        { 'C', new SvgSymbol(240, 0, "M330 40a30 30 0 1 1-60 0 30 30 0 1 1 60 0zm-70 40h80v30h-80z") },
-        { 'D', new SvgSymbol(360, 0, "M420 10l-5.69 56.81-20.19-53.4 9.22 56.37L370 23.41C385 55 395 80 395 110h50c0-30 10-55 25-86.6l-33.34 46.38 9.21-56.37-20.18 53.4z") },
-        { 'E', new SvgSymbol(0, 120, "M20 140v80h80v-80H20zm40 10h30v30H60v-30z") },
-        { 'F', new SvgSymbol(120, 120, "M200 130a30 30 0 0 0-27.06 42.94l-40 40a10 10 0 1 0 14.12 14.12l40-40A30 30 0 1 0 200 130z") },
-        { 'G', new SvgSymbol(240, 120, "M300 130a20 20 0 1 0 0 40 20 20 0 0 0 0-40zm0 40a30 30 0 1 0 0 60 30 30 0 0 0 0-60zm0 10a20 20 0 1 1 0 40 20 20 0 0 1 0-40z") },
-        { 'H', new SvgSymbol(360, 120, "M380 140h80v80h-30v-50h-50z") },
-        { 'X', new SvgSymbol(0, 240, "M60 250l-50 50h100zm0 50l-50 50h100z") },
-        { 'Y', new SvgSymbol(120, 240, "M220 350v-10h-30l40-40-50-50-50 50 40 40h-30v10h80z") },
-        { 'Z', new SvgSymbol(240, 240, "M250 250h100l-40 40v60h-20v-60z") }
-    };
 
     private IEnumerator rotator(int ix)
     {
@@ -411,7 +425,7 @@ public class PatternCubeModule : MonoBehaviour
 
 #pragma warning disable 414
 #pragma warning disable IDE0044 // Add readonly modifier
-    private readonly string TwitchHelpMessage = @"“!{0} place 1 in C” to place the first symbol in the box with letter C. “!{0} rotate 1 cw/ccw/180” to rotate a symbol. Chain with commas.";
+    private readonly string TwitchHelpMessage = @"!{0} rotate 1 cw/ccw/180 [rotate the first symbol] | !{0} place 1 in C [place the first symbol in the box with letter C] | Chain with commas.";
     private bool TwitchPlaysActive;
 #pragma warning restore IDE0044 // Add readonly modifier
 #pragma warning restore 414
